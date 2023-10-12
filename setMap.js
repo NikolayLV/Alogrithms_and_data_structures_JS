6c65059bcbbf8e7918564018871f5f1dfd31346f
NikolayLV <Nikolayd2002@gmail.com>
2023-10-12 09:23:52 +0200

    JS changes.

--- setMap.js
+++ setMap.js
@@ -1,16 +1,16 @@
-const map = new Map()
-const objKey = {id:5}
-map.set(objKey, "Nikolay LV")
+const map = new Map();
+const objKey = {id:5};
+map.set(objKey, "Nikolay LV");
 
 console.log(map.get(objKey));
 
-const set = new Set()
+const set = new Set();
 
-set.add(5)
-set.add(5)
-set.add(5)
-set.add(5)
-set.add(5)
-set.add(4)
-set.add(3)
-console.log(set)
+set.add(5);
+set.add(5);
+set.add(5);
+set.add(5);
+set.add(5);
+set.add(4);
+set.add(3);
+console.log(set);
