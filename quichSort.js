e4776ebe20a3f4314c6340e45db18146526291cf
Nikolay <120467269+NikolayLV@users.noreply.github.com>
2023-10-16 10:19:36 +0200

    JS changes.

--- quichSort.js
+++ quichSort.js
@@ -1,26 +1,26 @@
-const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23]
-let count = 0
+const arr = [0,3,2,5,6,8,23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,6,2,35,6,3,32,9,4,2,1,2,9,6,4,1,7,-1, -5, 23,9,4,2,1,2,9,6,4,1,7,-1, -5, 23];
+let count = 0;
 
 function quickSort(array) {
     if (array.length <= 1) {
-        return array
+        return array;
     }
     let pivotIndex = Math.floor(array.length / 2);
-    let pivot = array[pivotIndex]
-    let less = []
-    let greater = []
+    let pivot = array[pivotIndex];
+    let less = [];
+    let greater = [];
     for (let i = 0; i < array.length; i++) {
-        count += 1
+        count += 1;
         if(i === pivotIndex)
-            continue
+            continue;
         if (array[i] < pivot) {
-            less.push(array[i])
+            less.push(array[i]);
         } else {
-            greater.push(array[i])
+            greater.push(array[i]);
         }
     }
-    return [...quickSort(less), pivot, ...quickSort(greater)]
+    return [...quickSort(less), pivot, ...quickSort(greater)];
 }
 
-console.log(quickSort(arr))
-console.log('count', count)
+console.log(quickSort(arr));
+console.log('count', count);
